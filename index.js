b02ca82a56735383281567540cb4367679f2fd27
Steve Boissiere <sboissiere@meddbase.com>
2021-10-21 22:18:45 +0100

    Shameless pilfering of lunr indexing from hexo-theme-doc (https://github.com/zalando-incubator/hexo-theme-doc)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var lunr = require('lunr');
-var Entities = require('html-entities').AllHtmlEntities;
-
-var Html = new Entities();
+const lunr = require('lunr');
+const striptags = require('striptags');
+const cheerio = require('cheerio');
+const path = require('path');
+const slugid = require('github-slugid')
 
 var searchIndex;
 // Called with the `this` context provided by Gitbook
@@ -31,6 +32,13 @@ var documentsStore = {};
 var searchIndexEnabled = true;
 var indexSize = 0;
 
+function sanitize(html) {
+    return striptags(html, [], ' ').trim() // strip html tags
+        .replace(new RegExp('\n', 'g'), '') /* removes linebreaks   */ // eslint-disable-line no-control-regex
+        .replace(/ +(?= )/g, '') // removes double spaces
+        .replace(/&#123;|&quot;|&#125;|&lt;|&gt;/g, '');
+}
+
 module.exports = {
     book: {
         assets: './assets',
@@ -41,21 +49,16 @@ module.exports = {
 
     hooks: {
         // Index each page
-        'page': function(page) {
+        'page': function (page) {
             if (this.output.name != 'website' || !searchIndexEnabled || page.search === false) {
                 return page;
             }
 
-            var text, maxIndexSize;
-            maxIndexSize = this.config.get('pluginsConfig.lunr.maxIndexSize') || this.config.get('lunr.maxIndexSize');
+            const maxIndexSize = this.config.get('pluginsConfig.lunr.maxIndexSize') || this.config.get('lunr.maxIndexSize');
 
             this.log.debug.ln('index page', page.path);
 
-            text = page.content;
-            // Decode HTML
-            text = Html.decode(text);
-            // Strip HTML tags
-            text = text.replace(/(<([^>]+)>)/ig, '');
+            var text = sanitize(page.content);
 
             indexSize = indexSize + text.length;
             if (indexSize > maxIndexSize) {
@@ -81,11 +84,36 @@ module.exports = {
             documentsStore[doc.url] = doc;
             getSearchIndex(this).add(doc);
 
+            const $ = cheerio.load(page.content);
+
+            $('h1,h2').each(function () {
+                const elem = $(this);
+        
+                let body = [];
+        
+                elem.nextUntil('h2').each(function () {
+                  body.push($(this).html());
+                });
+        
+                body = body.join(' ');
+        
+                var subdoc = {
+                    url: `${doc.url}#${elem.attr('id') || slugid(elem.text())}`,
+                    title: sanitize(elem.html()),
+                    summary: page.description,
+                    keywords: '',
+                    body: sanitize(body)
+                };
+    
+                documentsStore[subdoc.url] = subdoc;
+                getSearchIndex(this).add(subdoc);
+              });                    
+
             return page;
         },
 
         // Write index to disk
-        'finish': function() {
+        'finish': function () {
             if (this.output.name != 'website') return;
 
             this.log.debug.ln('write search index');
